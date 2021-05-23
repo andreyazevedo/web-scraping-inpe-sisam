f015bbdbe66cd370075d0fca71dbaf4c414dc40c
Andrey Azevedo <andreyaze@gmail.com>
2021-05-23 20:34:42 -0300

    feat: adiciona barra de progresso

--- src/service.js
+++ src/service.js
@@ -1,5 +1,7 @@
 import stringHash from 'string-hash';
+import ProgressBar from 'cli-progress';
 import { fetchJSON } from './http';
+import config from './states.json';
 import { fmtStates, fmtPeriod, readFile, fmtApiUrl, writeToFile } from './helpers';
 
 export const fetchSisamApi = async url => {
@@ -25,22 +27,28 @@ const getDataFromState = async (state, period) => {
   const firstPage = await fetchSisamApi(url);
   const { pages } = firstPage;
   const listOfPages = Array.from(Array(pages).keys());
+  const stateSlug = config[state].slug; 
 
-  return Promise.all(listOfPages.map((_, index) => {
+  const opt = {
+    format: `Downloading: [{bar}] {percentage}% |[${stateSlug}]  ${period.start} - ${period.end} | pages: {value}/{total}`,
+  }
+  const progressUI = new ProgressBar.SingleBar(opt, ProgressBar.Presets.shades_classic);
+  progressUI.start(listOfPages.length, 0);
+
+  const result = await Promise.all(listOfPages.map((_, index) => {
     const current = index + 1;
     const url = fmtApiUrl(state, period, current);
-    console.log(`>>> start request: state: ${state}, page: ${current}`);
-    return fetchSisamApi(url).then(data => {
-      const fromCache = data.fromCache;
-      const status = data.error ? `FAILED - ${url}` : 'SUCCESS';
 
-      if (!fromCache) {
-        console.log(`>>> end request: state: ${state}, page: ${current} - ${status}`);
-      }
+    return fetchSisamApi(url).then(data => {
+      progressUI.increment();
 
       return data;
     });
   }));
+
+  progressUI.stop();
+
+  return result;
 };
 
 export const scrapSisamData = async () => {
